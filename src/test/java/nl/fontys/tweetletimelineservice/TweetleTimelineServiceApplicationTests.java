bdadb8fb281883a72a17b91e450999babf83b1c5
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-04 15:17:23 +0100

    add test

--- src/test/java/nl/fontys/tweetletimelineservice/TweetleTimelineServiceApplicationTests.java
+++ src/test/java/nl/fontys/tweetletimelineservice/TweetleTimelineServiceApplicationTests.java
@@ -2,8 +2,10 @@ package nl.fontys.tweetletimelineservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class TweetleTimelineServiceApplicationTests {
 
     @Test
