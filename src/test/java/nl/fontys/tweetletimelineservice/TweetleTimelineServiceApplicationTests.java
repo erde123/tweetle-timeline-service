d99c06020aa4276b5534b6365c76fd3dd5108132
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-04 13:19:05 +0100

    init

---
+++ src/test/java/nl/fontys/tweetletimelineservice/TweetleTimelineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.tweetletimelineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetleTimelineServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
