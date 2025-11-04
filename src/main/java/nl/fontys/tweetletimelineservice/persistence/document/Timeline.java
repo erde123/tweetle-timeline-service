256549ef96b06a49f303ba046d2fd827dc63e285
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-04 13:18:46 +0100

    add timeline domain

---
+++ src/main/java/nl/fontys/tweetletimelineservice/persistence/document/Timeline.java
@@ -0,0 +1,34 @@
+package nl.fontys.tweetletimelineservice.persistence.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "timelines")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Timeline {
+    @Id
+    private String id;
+    private Long userId;
+    private List<TweetRef> tweets;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class TweetRef {
+        private String tweetId;
+        private Long authorId;
+        private long createdAt;
+    }
+}
+
